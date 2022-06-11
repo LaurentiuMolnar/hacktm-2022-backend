40e2c00de7adc093e2b39ce0f268e6d6df15d819
Laurentiu Molnar <lau.molnar2013@gmail.com>
2022-06-11 11:59:25 +0300

    Initial project setup

---
+++ test/hacktm-2022-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Hacktm2022Backend from '../lib/hacktm-2022-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hacktm-2022-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Hacktm2022Backend.Hacktm2022BackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
