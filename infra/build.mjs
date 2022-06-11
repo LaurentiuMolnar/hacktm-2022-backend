51f705393f4db731328966da022cc6bfc134e147
Laurentiu Molnar <lau.molnar2013@gmail.com>
2022-06-11 17:04:55 +0300

    Update build script

--- infra/build.mjs
+++ infra/build.mjs
@@ -1,15 +1,19 @@
 import fs from 'fs';
 import path from 'path';
 
+import rimraf from 'rimraf';
 import { buildSync } from 'esbuild';
 
 const subDirs = fs.readdirSync(path.resolve('src'));
 
-buildSync({
-  entryPoints: subDirs.map((dir) => path.resolve('src', dir, 'index.ts')),
-  bundle: true,
-  treeShaking: true,
-  outdir: 'dist',
-  target: ['node16'],
-  format: 'cjs',
+rimraf(path.resolve('dist'), {}, () => {
+  buildSync({
+    entryPoints: subDirs.map((dir) => path.resolve('src', dir, 'index.ts')),
+    platform: 'node',
+    bundle: true,
+    treeShaking: true,
+    outdir: 'dist',
+    target: ['node16'],
+    format: 'cjs',
+  });
 });
